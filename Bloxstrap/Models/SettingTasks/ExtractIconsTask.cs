2dca0792b91d19c834047473d78a0afe22c9eead
meddsam <136804886+Meddsam@users.noreply.github.com>
2025-05-21 14:06:06 +0100

    Fixed extracting icons issue in shortcuts

--- Bloxstrap/Models/SettingTasks/ExtractIconsTask.cs
+++ Bloxstrap/Models/SettingTasks/ExtractIconsTask.cs
@@ -6,6 +6,19 @@ namespace Bloxstrap.Models.SettingTasks
     {
         private string _path => Path.Combine(Paths.Base, Strings.Paths_Icons);
 
+        // List of embedded icon resource names to extract
+        private static readonly string[] AllowedIconNames =
+        {
+            "Icon2008.ico",
+            "Icon2011.ico",
+            "Icon2017.ico",
+            "Icon2019.ico",
+            "Icon2022.ico",
+            "IconBloxstrap.ico",
+            "IconEarly2015.ico",
+            "IconLate2015.ico"
+        };
+
         public ExtractIconsTask() : base("ExtractIcons")
         {
             OriginalState = Directory.Exists(_path);
@@ -18,18 +31,22 @@ namespace Bloxstrap.Models.SettingTasks
                 Directory.CreateDirectory(_path);
 
                 var assembly = Assembly.GetExecutingAssembly();
-                var resourceNames = assembly.GetManifestResourceNames().Where(x => x.EndsWith(".ico"));
+                var resourceNames = assembly.GetManifestResourceNames();
 
-                foreach (string name in resourceNames)
+                foreach (string iconName in AllowedIconNames)
                 {
-                    string path = Path.Combine(_path, name.Replace("Bloxstrap.Resources.", ""));
-                    var stream = assembly.GetManifestResourceStream(name)!;
+                    string fullResourceName = $"Bloxstrap.Resources.{iconName}";
 
+                    if (!resourceNames.Contains(fullResourceName))
+                        continue;
+
+                    using var stream = assembly.GetManifestResourceStream(fullResourceName)!;
                     using var memoryStream = new MemoryStream();
                     stream.CopyTo(memoryStream);
 
-                    Filesystem.AssertReadOnly(path);
-                    File.WriteAllBytes(path, memoryStream.ToArray());
+                    string filePath = Path.Combine(_path, iconName);
+                    Filesystem.AssertReadOnly(filePath);
+                    File.WriteAllBytes(filePath, memoryStream.ToArray());
                 }
             }
             else if (Directory.Exists(_path))
